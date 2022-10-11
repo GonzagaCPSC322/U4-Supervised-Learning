04b3126caa14a4428631a8fd17157e121ef4f564
Gina Sprint <sprint@gonzaga.edu>
2022-10-11 18:08:48 +0000

    knn starter code and hints

--- ClassificationFun/main.py
+++ ClassificationFun/main.py
@@ -1,6 +1,22 @@
+import operator
 import numpy as np
+from scipy.spatial.distance import euclidean
 
 from mysimplelinearregressor import MySimpleLinearRegressor
+
+def compute_euclidean_distance(v1, v2):
+    return np.sqrt(sum([(v1[i] - v2[i]) ** 2 for i in range(len(v1))]))
+
+def test_compute_euclidean_distance():
+    # we need some data
+    np.random.seed(0)
+    v1 = list(np.random.random(100))
+    v2 = list(np.random.random(100))
+
+    dist = compute_euclidean_distance(v1, v2)
+    # test against scipy
+    dist_sol = euclidean(v1, v2)
+    assert np.isclose(dist, dist_sol)
 
 def main():
     # starting with PA4, we are going to be implementing
@@ -60,6 +76,44 @@ def main():
 
     # now, lets rework this example into unit testing form
     
+    # kNN starter code and hints
+    header = ["att1", "att2"]
+    X_train = [
+        [3, 2],
+        [6, 6],
+        [4, 1],
+        [4, 4],
+        [1, 2],
+        [2, 0],
+        [0, 3],
+        [1, 6]
+    ]
+    y_train = ["no", "yes", "no", "no", "yes", "no", "yes", "yes"] # parallel to X_train
+    test_instance = [2, 3]
+
+    # calculate the distances between each train instance and the test
+    # instance
+    row_indexes_dists = []
+    for i, train_instance in enumerate(X_train):
+        dist = compute_euclidean_distance(train_instance, test_instance)
+        row_indexes_dists.append((i, dist))
+
+    for row in row_indexes_dists:
+        print(row)
+    # now we need the k smallest distances
+    # we can sort row_indexes_dists by distance
+    row_indexes_dists.sort(key=operator.itemgetter(-1)) # -1 or 1
+    # because the distance is at the index in each item (list)
+    
+    # now, grab the top k
+    k = 3
+    top_k = row_indexes_dists[:k]
+    print("top k:")
+    for row in top_k:
+        print(row)
+    # TODO: extract the top k closes neighbors' y labels from y_train
+    # then use majority voting to find the prediction for this test instance
+    # can make use of get_frequencies()
 
 
 if __name__ == "__main__":
