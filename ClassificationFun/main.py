a684d33f0e684d558410a5e831cfd7f79413d578
Gina Sprint <sprint@gonzaga.edu>
2024-10-08 10:41:52 -0700

    PA4 knn starter code

--- ClassificationFun/main.py
+++ ClassificationFun/main.py
@@ -1,6 +1,24 @@
+import operator
 import numpy as np
+from scipy.spatial.distance import euclidean
+from sklearn.neighbors import KNeighborsClassifier
 
 from mysimplelinearregressor import MySimpleLinearRegressor
+
+# unit under test
+def compute_euclidean_distance(v1, v2):
+    return np.sqrt(sum([(v1[i] - v2[i]) ** 2 for i in range(len(v1))]))
+
+# unit test
+def test_compute_euclidean_distance():
+    # we need data
+    np.random.seed(0)
+    v1 = np.random.random(100)
+    v2 = np.random.random(100)
+    dist = compute_euclidean_distance(v1, v2)
+    # let's test against scipy
+    sp_dist = euclidean(v1, v2)
+    assert np.isclose(dist, sp_dist)
 
 def main():
     # starting with PA4, we are going to implement
@@ -59,6 +77,46 @@ def main():
 
     # lets convert this code to unit tests!
 
+    # PA4 kNN starter code
+    header = ["att1", "att2"]
+    X_train = [
+        [3, 2],
+        [6, 6],
+        [4, 1],
+        [4, 4],
+        [1, 2],
+        [2, 0],
+        [0, 3],
+        [1, 6]
+    ]
+    y_train = ["no", "yes", "no", "no", "yes", "no", "yes", "yes"] # parallel to X_train
+    unseen_instance = [2, 3]
+    # 1. normalize if needed (not needed for dataset, assumed [0, 10])
+    # 2. calculate distances between each training set instance
+    # and the unseen instance
+    row_indexes_dists = [] # list of tuple pairs (row index, dist)
+    for i, row in enumerate(X_train):
+        dist = compute_euclidean_distance(row, unseen_instance)
+        row_indexes_dists.append((i, dist))
+    # need to sort row_indexes_dists by dist
+    print(row_indexes_dists)
+    row_indexes_dists.sort(key=operator.itemgetter(-1)) # get the item
+    # in the tuple at index -1 and use that for sorting
+    k = 3 
+    top_k = row_indexes_dists[:k]
+    for row in top_k:
+        print(row)
+
+    # TODO: extract the top k closes neighbors' y labels from y_train
+    # then use majority voting to find the prediction for this test instance
+    # can make use of get_frequencies()
+
+    # check work against sci-kit learn
+    knn_clf = KNeighborsClassifier(n_neighbors=3, metric="euclidean")
+    knn_clf.fit(X_train, y_train)
+    distances, indexes = knn_clf.kneighbors([unseen_instance])
+    print(distances)
+    print(indexes)
 
 if __name__ == "__main__":
     main()
